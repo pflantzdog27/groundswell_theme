7a955c2cdc39f89fd4df93c223597fade14366f3
Adam Pflantzer <apflantzer@gmail.com>
2015-03-12 09:50:20 -0400

    GTM Updates

--- section-layouts/components/social-share-bar.php
+++ section-layouts/components/social-share-bar.php
@@ -1,6 +1,8 @@
 <div class="social-link-bar">
     <ul class="list-unstyled col-sm-8 col-sm-offset-2">
-        <li class="facebook col-xs-4"><a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href),'facebook-share-dialog','width=626,height=436'); //_gaq.push(['_trackEvent', 'Facebook', // 'Facebook-Top Single Post Page', '<?php //the_title();?>']);return false;">
+        <li class="facebook col-xs-4"><a class="facebook-share" href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(location.href),'facebook-share-dialog','width=626,
+            height=436'); //_gaq.push(['_trackEvent', 'Facebook',
+        // 'Facebook-Top Single Post Page', '<?php //the_title();?>']);return false;">
                 <i class="icon-facebook"></i><span class="hidden-xs"> Share</span>
         </a></li>
         <li class="twitter col-xs-4"><a href="http://twitter.com/intent/tweet?url=&text=<?php the_title();?>&via=groundswell" target="_blank">
