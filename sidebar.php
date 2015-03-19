3141818648e267b1e44fe859a6cfb5d5cb13e7d7
Adam Pflantzer <apflantzer@gmail.com>
2015-03-19 19:10:20 -0400

    Related Posts

--- sidebar.php
+++ sidebar.php
@@ -4,9 +4,12 @@
         $all_cats_of_post = get_the_category($post_ID);
         for($i = 0; $i < sizeof($all_cats_of_post); $i++) { ?>
             <div class="sidebar-widget">
-                <h2 class="widget-title">Related<?php //echo $all_cats_of_post[$i]->cat_name; ?> Posts</h2>
                 <ul>
-                    <?php global $post; $cat_posts = get_posts('numberposts=4&exclude='.$post_ID.'&category='.$all_cats_of_post[$i]->cat_ID); foreach($cat_posts as $post) : ?>
+                    <?php global $post; $cat_posts = get_posts('numberposts=4&exclude='.$post_ID.'&category='.$all_cats_of_post[$i]->cat_ID);
+                    if($cat_posts) { ?>
+                        <h2 class="widget-title">Related Posts</h2>
+                    <?php }
+                    foreach($cat_posts as $post) : ?>
                         <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
                     <?php endforeach; ?>
                 </ul>
