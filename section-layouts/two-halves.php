e9658390bcd5a73e7d23d48ff74927a414d27ccd
Adam Pflantzer <apflantzer@gmail.com>
2015-03-28 09:50:53 -0400

    Mobile Updates

--- section-layouts/two-halves.php
+++ section-layouts/two-halves.php
@@ -8,7 +8,8 @@
         <?php endif; /* END section title */ ?>
         <?php include('components/left-half.php'); ?>
     </div>
-    <div class="section-right-column <?php echo (!$sectionDividerCheck ? 'parallax' : false);?> col-sm-6" <?php echo ($rightBlock == 'Petition App' ? 'id="petition-wrap"' : false);?> <?php echo ($rightBlock == 'Email Signup' ? 'id="email-subscription"' : false);?>>
+    <div class="section-right-column <?php echo (!$sectionDividerCheck && $rightBlock != 'Petition App' ? 'parallax' : false);?> col-sm-6" <?php echo ($rightBlock == 'Petition App' ? 'id="petition-wrap"' : false);?> <?php echo ($rightBlock ==
+    'Email Signup' ? 'id="email-subscription"' : false);?>>
         <?php include('components/right-half.php'); ?>
     </div>
 </div>
