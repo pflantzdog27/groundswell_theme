3914ccfc7ae4a05f19f04ebc3be828db9fb0e201
Adam Pflantzer <apflantzer@gmail.com>
2015-04-13 16:36:39 -0400

    underline topic links

--- section-layouts/single-inspiration.php
+++ section-layouts/single-inspiration.php
@@ -43,11 +43,12 @@
                     <strong>Topics: </strong>
                     <?php
                     $categories = get_the_category();
-                    $separator = ',';
+                    $separator = ', ';
                     $output = '';
                     if($categories){
                         foreach($categories as $category) {
-                            $output .= '<a href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name.'</a>'.$separator;
+                            $output .= '<a style="text-decoration: underline" href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ),
+                                    $category->name ) ) . '">'.$category->cat_name.'</a>'.$separator;
                         }
                         echo trim($output, $separator);
                     }
