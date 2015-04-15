4a5706bc76e81d3e509eddcaa29a9cd41d5e3a01
Adam Pflantzer <apflantzer@gmail.com>
2015-04-15 10:53:11 -0400

    Standard Page Format

--- page.php
+++ page.php
@@ -1,8 +1,17 @@
 <?php get_header(); ?>
 
     <?php if (have_posts()) : while (have_posts()) : the_post();?>
+    <header class="page-header">
+        <div class="container">
+            <h1><?php the_title(); ?></h1>
+        </div>
+    </header>
 
-        <?php the_content(); ?>
+    <section class="content-section">
+        <div class="container">
+            <?php the_content(); ?>
+        </div>
+    </section>
 
     <?php endwhile; endif; ?>
 
