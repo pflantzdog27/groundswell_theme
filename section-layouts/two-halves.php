c8adccf5838512735a648d8999c75847ca1c8150
Adam Pflantzer <apflantzer@gmail.com>
2015-04-19 22:52:48 -0400

    Fix title in two halves

--- section-layouts/two-halves.php
+++ section-layouts/two-halves.php
@@ -1,11 +1,8 @@
 <div class="section-template row">
-    <?php if( $sectionDividerCheck && $sectionTitle ) : ?>
+    <?php if( !$sectionDividerCheck && $sectionTitle ) : ?>
             <h2 class="section-title"><?php echo $sectionTitle; ?></h2>
     <?php endif; /* END section title */ ?>
     <div class="section-left-column col-sm-6">
-        <?php if(!$sectionDividerCheck && $sectionTitle) : ?>
-            <h2 class="section-title"><?php echo $sectionTitle; ?></h2>
-        <?php endif; /* END section title */ ?>
         <?php include('components/left-half.php'); ?>
     </div>
     <div class="section-right-column <?php echo (!$sectionDividerCheck && $rightBlock != 'Petition App' ? 'parallax' : false);?> col-sm-6" <?php echo ($rightBlock == 'Petition App' ? 'id="petition-wrap"' : false);?> <?php echo ($rightBlock ==
