f34cd410bb7835f50977d6ed2c74eb87c3fd7d9f
Adam Pflantzer <apflantzer@gmail.com>
2015-04-20 13:13:41 -0400

    Search function update

--- search.php
+++ search.php
@@ -5,33 +5,19 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-8">
-                    <h1>Blog Search Results for : <b><?php echo get_search_query(); ?></b></h1>
+                    <h1>Search Results for : <b><?php echo get_search_query(); ?></b></h1>
                 </div>
             </div>
         </div>
     </header>
-    <section id="blog" class="content-section">
+    <section class="content-section">
         <div class="container">
 			<?php if ( have_posts() ) { ?>
-				<div class="section-template parallax three-column-section row">
-					<?php while ( have_posts() ) : the_post();?>
-						<div class="col-sm-4 blog-post" id="post-<?php the_ID();?>">
-							<article>
-								<span class="<?php the_field('blog_posts_post_type');?>"></span>
-								<figure>
-									<a href="<?php the_permalink();?>">
-										<?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-											the_post_thumbnail('blog');
-										} ?>
-										<figcaption>
-											<h3><?php the_title();?></h3>
-										</figcaption>
-									</a>
-								</figure>
-							</article>
-						</div>
-					<?php endwhile; ?>						
-				</div>
+                <ul class="list-unstyled">
+                    <?php while ( have_posts() ) : the_post();?>
+                        <li><a href="<?php the_permalink();?>"><?php the_title(); ?></a></li>
+                    <?php endwhile; ?>
+                </ul>
 				<nav style="text-align: center;">
 					<?php wpbeginner_numeric_posts_nav(); ?>
 				</nav>
