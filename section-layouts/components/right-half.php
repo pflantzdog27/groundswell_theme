cb98d44e4f2a0c6e558d1a00136f649660bad1e0
Adam Pflantzer <apflantzer@gmail.com>
2015-04-27 11:13:50 -0400

    Button updates - in section social

--- section-layouts/components/right-half.php
+++ section-layouts/components/right-half.php
@@ -8,7 +8,7 @@
     include('email-input-form.php');
 } elseif($rightBlock == 'Social Buttons') { ?>
     <div class="in-section-social">
-        <div style="background : #3b5998"><div class="fb-like" data-href="https://www.facebook.com/GroundswellMovement" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div></div>
-        <button style="background : #eaeaea; padding: 8px 12px 2px;"><a class="twitter-follow-button" href="https://twitter.com/GroundswellMvmt" data-show-count="true" data-show-screen-name="false" data-lang="en">Follow @GroundswellMvmt</a></button>
+        <div class="btn gs-btn" style="background : #3b5998"><div class="fb-like" data-href="https://www.facebook.com/GroundswellMovement" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div></div>
+        <button class="btn gs-btn" style="background : #eaeaea; padding: 8px 12px 2px;"><a class="twitter-follow-button" href="https://twitter.com/GroundswellMvmt" data-show-count="true" data-show-screen-name="false" data-lang="en">Follow @GroundswellMvmt</a></button>
     </div>
 <?php } ?>
