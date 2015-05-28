cf681264d191ca8741e41723572b8fdac8b687ae
Adam Pflantzer <apflantzer@gmail.com>
2015-05-27 22:08:10 -0400

    404 update

--- 404.php
+++ 404.php
@@ -13,9 +13,9 @@
         <div class="container">
             <h2>Looks like this page doesn't exist, but there are some other really good ones. You can head back to the <a style="color: #ea5a3a;" href="<?php echo get_home_url(); ?>">homepage</a> or try one of these links:</h2>
             <ul class="list-unstyled">
-                <li><a href="#">Campaign Partnerships</a></li>
-                <li><a href="#">Learning</a></li>
-                <li><a href="#">Digital Tools</a></li>
+                <li><a href="/campaign-partnerships">Campaign Partnerships</a></li>
+                <li><a href="/learning">Learning</a></li>
+                <li><a href="/digital-tools">Digital Tools</a></li>
             </ul>
         </div>
     </section>
