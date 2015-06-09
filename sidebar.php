f848272e1d23f80223f3f543b8ac5898feeafba5
Adam Pflantzer <apflantzer@gmail.com>
2015-06-09 13:40:51 -0400

    update per post number

--- sidebar.php
+++ sidebar.php
@@ -5,8 +5,8 @@
         <?php  $post_ID = $wp_query->posts[0]->ID;
         $all_cats_of_post = get_the_category($post_ID);
         for($i = 0; $i < sizeof($all_cats_of_post); $i++) { ?>
-            <?php global $post; $cat_posts = get_posts('numberposts=3&exclude='.$post_ID.'&category='.$all_cats_of_post[0]->cat_ID);
-            if($cat_posts && $all_cats_of_post[0]->cat_ID != 27 && $all_cats_of_post[0]->cat_ID != 26 && $all_cats_of_post[0]->cat_ID != 28 && $all_cats_of_post[0]->cat_ID != 29 && $all_cats_of_post[0]->cat_ID != 1) { ?>
+            <?php global $post; $cat_posts = get_posts('numberposts=3&exclude='.$post_ID.'&category='.$all_cats_of_post[i]->cat_ID);
+            if($cat_posts && $all_cats_of_post[i]->cat_ID != 27 && $all_cats_of_post[i]->cat_ID != 26 && $all_cats_of_post[i]->cat_ID != 28 && $all_cats_of_post[i]->cat_ID != 29 && $all_cats_of_post[i]->cat_ID != 1) { ?>
                 <div class="sidebar-widget">
                     <ul>
                         <h2 class="widget-title">On <?php echo $all_cats_of_post[$i]->cat_name; ?></h2>
